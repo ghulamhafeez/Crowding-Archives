847c364046ba66442b1743ccecd263a2c73184fb
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-07-19 17:59:33 +0500

    Create Project & set app

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Grid } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Grid>
+
+    </Grid>
+  );
+}
